ec9cad0cc18e1f5d5b90fed3276eb9a5d16aa746
adpardo1 <73541227+adpardo1@users.noreply.github.com>
2021-06-24 15:41:30 -0500

    Add files via upload

---
+++ Taller11POO/src/paquete1/Main.java
@@ -0,0 +1,12 @@
+package paquete1;
+
+/**
+ *
+ * @author David Pardo
+ */
+public class Main {
+    public static void main(String[] args) {
+        
+    }
+    
+}
---
+++ Taller11POO/src/paquete2/Menu.java
@@ -0,0 +1,10 @@
+package paquete2;
+
+/**
+ *
+ * @author David Pardo
+ */
+public abstract class Menu {
+
+    
+}
